5abb7241e503c2ac5e1d73acdb594a27b1f7a893
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2025-03-17 13:19:44 +0530

    initial-commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Integrated DVC pipeline with the entire Project pipeline',
+    author='Shrijeet Kumar',
+    license='',
+)
